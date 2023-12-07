8ca3cb79b9c019a6fca1d0369b2fdd3eba282050
Jason Foutz <jfoutz@gmail.com>
2023-12-06 16:47:10 -0800

    playing with modules

--- flake.nix
+++ flake.nix
@@ -28,8 +28,8 @@
       devShells = forAllSystems ({ pkgs }: {
         default =
           let
-            # Use Python 3.10
-            python = pkgs.python310;
+            # Use Python 3.11
+            python = pkgs.python311;
           in
           pkgs.mkShell {
             # The Nix packages provided in the environment
